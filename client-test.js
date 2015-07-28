324b4f0bae91ef6622ec61c0cec1dbf299ce005a
Damien Clark <damo.clarky@gmail.com>
2015-07-29 09:20:48 +1000

    Move onMessage and emitMessage methods from BuzzerServer into MessageFactory.  The socket object is augmented with these methods by calling the MessageFactory.updateSocket function.
    Simplifies the design considerably and makes these two methods re-usable between server and client.  The MessageFactory class will be used by the client as well.

--- client-test.js
+++ client-test.js
@@ -6,31 +6,7 @@ var socketio = require('socket.io-client')('http://localhost:3000') ;
 socketio.on('connect', function()
 {
 	console.log('Connected') ;
-	/**
-	 * Establish event handler for receiving an AbstractMessage message - overriden
-	 * method from Socket class
-	 *
-	 * @param {String} event Name of event to wait for
-	 * @param {Function} fn Function to call on message
-	 */
-	socketio.onMessage = function(event,fn)
-	{
-		this.on(event,function(msg)
-		{
-			var message = MessageFactory.restore(msg) ;
-			fn(message) ;
-		} ) ;
-	} ;
-	
-	/**
-	 * Emit an AbstractMessage message - overidden method from Socket class
-	 * @param {AbstractMessage} message A subclass of AbstractMessage to be sent
-	 * @param {Function} fn An optional callack to execute to send a reply
-	 */
-	socketio.emitMessage = function(message,fn)
-	{
-		this.emit(message.getType(),message.getJSON(),fn) ;
-	} ;
+	MessageFactory.updateSocket(socketio) ;
 
 	socketio.onMessage(MessageFactory.JOINGAMEMESSAGE,function(message)
 	{
