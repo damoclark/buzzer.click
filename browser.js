f9aa5f5842c1f78d85df7da045172f72720fc3a1
Damien Clark <damo.clarky@gmail.com>
2015-07-30 14:05:55 +1000

    Implement ConfirmMessage class, as a generic message object to send back in response to a command message ConfirmMessage.js, BuzzerServer.js, browser.js, MessageFactory.js
    For Message objects, copy the data prototype and schema prototype into the instance so that each instance has its own separate copy
    AbstractMessage.js
    
    Refactored the validate method of AbstractMessage to use getJSON() to generate the object to be sent over the wire, rather than reimplementing it there.  Took away ability to validate a parameter passed in (can add back if needed later).
    AbstractMessage.js
    
    Added debugging code
    AbstractMessage.js, BuzzerServer.js, browser.js,
    
    Updated set method of AbstractMessage to always return true if nothing passed in, but to call validate if something was passed in.
    AbstractMessage.js
    
    Improve commenting
    AbstractMessage.js

--- browser.js
+++ browser.js
@@ -9,21 +9,28 @@ socketio.on('connect', function()
 	console.log('Connected') ;
 	MessageFactory.updateSocket(socketio) ;
 
-	socketio.onMessage(MessageFactory.JOINGAMEMESSAGE,function(message)
+	//socketio.onMessage(MessageFactory.JOINGAMEMESSAGE,function(message)
+	//{
+	//		console.log('Received '+message.getType()+' message with data:'+JSON.stringify(message.data)) ;
+	//} ) ;
+
+	socketio.onMessage(MessageFactory.CONFIRMMESSAGE,function(message)
 	{
-			console.log('Received '+message.getType()+' message with data:'+JSON.stringify(message.data)) ;
-	} ) ;
+		console.log('Received '+message.getType()+' message with msg:'+JSON.stringify(message)) ;
+		console.log('Confirm code='+message.getCode()+', and message='+message.getMessage()) ;
+	}) ;
 
 	var m ;
 	var settings = new Settings() ;
 	settings.setName('Fun Game') ;
 	m = MessageFactory.create(MessageFactory.CREATEGAMEMESSAGE,settings) ;
 	socketio.emitMessage(m) ;
-	m = MessageFactory.create(MessageFactory.JOINGAMEMESSAGE) ;
-	m.setGameCode('12345') ;
-	socketio.emitMessage(m) ;
 
-	m = new MessageFactory.JoinGameMessage({"gamecode":"54321"}) ; 
-	
-	socketio.emitMessage(m) ;
+	//m = MessageFactory.create(MessageFactory.JOINGAMEMESSAGE) ;
+	//m.setGameCode('12345') ;
+	//socketio.emitMessage(m) ;
+	//
+	//m = new MessageFactory.JoinGameMessage({"gamecode":"54321"}) ; 
+	//
+	//socketio.emitMessage(m) ;
 } ) ;
