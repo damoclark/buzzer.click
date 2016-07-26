3d2a768a74751069e82a53bdce7126a8bdbfad6f
Wade <wade@boffindevelopments.com>
2016-07-26 22:08:46 +1000

    Updates beautify-solution to ES6

--- beautify-solution.js
+++ beautify-solution.js
@@ -1,45 +1,46 @@
-var fs = require('fs');
-var path = require('path');
-var beautify = require('js-beautify').js;
-var beautifyOptions = JSON.parse(fs.readFileSync(path.join(__dirname,
+let fs = require('fs');
+let path = require('path');
+let beautify = require('js-beautify').js;
+let beautifyOptions = JSON.parse(fs.readFileSync(path.join(__dirname,
     '.jsbeautifyrc'), 'utf8'));
-var ignoreFolders = ['$Recycle.Bin', '.vscode', 'node_modules', 'typings',
+let ignoreFolders = ['$Recycle.Bin', 'node_modules', 'typings',
     '.git'
 ];
-var ignoreFiles = ['bundle.js'];
+let ignoreFiles = ['bundle.js'];
 /**
  * Gets all files where the extions is `.js` and the filename
  * does not start with a `.`. This method is recusive, so all
  * child folders are included in the search too.
  * @param  {} dirPath
  */
-var getJsFilesRecursive = function(dirPath) {
-    var files = [];
-    var dirContents = fs.readdirSync(dirPath);
-    for (var i = 0; i < dirContents.length; i++) {
-        var element = dirContents[i];
-        var elementStat = fs.statSync(path.join(dirPath, element));
+let getJsFilesRecursive = function(dirPath) {
+    let files = [];
+    let dirContents = fs.readdirSync(dirPath);
+    dirContents.forEach(element => {
+        let elementStat = fs.statSync(path.join(dirPath, element));
+        if (element.startsWith('.')) {
+            return;
+        }
         if (elementStat.isDirectory()) {
             if (ignoreFolders.indexOf(element) >= 0) {
-                continue;
+                return;
             }
-            files = files.concat(getJsFilesRecursive(path.join(dirPath, element)));
+            files = files.concat(getJsFilesRecursive(path.join(dirPath,
+                element)));
         } else {
-            if (ignoreFiles.indexOf(element) >= 0) {
-                continue;
-            }
-            if (element.startsWith('.') || !element.endsWith('.js')) {
-                continue;
+            if (ignoreFiles.indexOf(element) >= 0 || !element.endsWith(
+                    '.js')) {
+                return;
             }
             files.push(path.join(dirPath, element));
         }
-    }
+    });
     return files;
 };
-var files = getJsFilesRecursive(__dirname);
-for (var i = 0; i < files.length; i++) {
-    var filePath = files[i];
-    var beautifiedFileContents = beautify(fs.readFileSync(filePath, 'utf8'),
+
+let files = getJsFilesRecursive(__dirname);
+files.forEach(filePath => {
+    let beautifiedFileContents = beautify(fs.readFileSync(filePath, 'utf8'),
         beautifyOptions);
     fs.writeFileSync(filePath, beautifiedFileContents, 'utf8');
-}
+});
