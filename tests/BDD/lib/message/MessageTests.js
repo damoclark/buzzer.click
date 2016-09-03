ab0069ed987b624126b2535697f6cebedbd44f26
Nick Andjelkovic <nenad.andjelkovic@cqumail.com>
2016-09-03 19:43:00 +1000

    added the rest of the tests for confirmmessage

--- tests/BDD/lib/message/MessageTests.js
+++ tests/BDD/lib/message/MessageTests.js
@@ -9,8 +9,7 @@ describe('ConfirmMessage', function() {
         it('should set value',
             function () {
                 var c = new ConfirmMessage();
-                
-                c.buzzerError = new Error("test");
+                c.buzzerError = new Error('test');
                 c.message.should.not.be.null();
                 c.message.should.be.equal('test');
             });
@@ -18,8 +17,48 @@ describe('ConfirmMessage', function() {
             function () {
                 var c = new ConfirmMessage();
                 (function () {
-                    var x = c.buzzerError
+                    var x = c.buzzerError;
                 }).should.throw();
             });
+    });
+    describe('#message', function () {
+        it('should set value',
+            function () {
+                var c = new ConfirmMessage();
+                c.message = 'test';
+                c.data.confirm._message.should.not.be.null();
+                c.data.confirm._message.should.be.equal('test');
+            });
+        it('should get value',
+            function () {
+                var c = new ConfirmMessage();
+                c.data.confirm._message = 'test';
+                c.message.should.not.be.null();
+                c.message.should.be.equal('test');
+            }
+        );
+    });
+    describe('#code', function () {
+        it('should set value, be a number and set message to default message', function () {
+            var c = new ConfirmMessage();
+            c.code = 0;
+            c.data.confirm._code.should.not.be.null();
+            c.data.confirm._code.should.be.equal(0).and.a.number;
+            c.data.confirm._message.should.not.be.null;
+        });
+        it('should set value and message',function () {
+            var c = new ConfirmMessage();
+            c.code = 0;
+            c.message = 'New';
+            c.data.confirm._code.should.not.be.null();
+            c.data.confirm._code.should.be.equal(0).and.a.number;
+            c.data.confirm._message.should.be.equal('New');
+        });
+        it('should get value', function () {
+            var c = new ConfirmMessage();
+            c.data.confirm._code = 2;
+            c.code.should.not.be.null;
+            c.code.should.be.equal(2).and.a.number;
+        });
     });
 });
