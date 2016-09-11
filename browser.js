941650816a5d3f1647c906a9183ef9e5ee8e7e59
Wade <wade@boffindevelopments.com>
2016-09-11 12:22:46 +1000

    Add example CreateSession

---
+++ browser.js
@@ -0,0 +1,6 @@
+module.exports = {
+    Sessions: require('./lib/Sessions'),
+    Settings: require('./lib/Settings'),
+    messageFactory: require('./lib/MessageFactory'),
+    constants: require('./lib/Constants')
+};
