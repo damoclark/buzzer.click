1ee3f90b16f55ed3111978ed088b0132ed1903f8
Wade <wade@boffindevelopments.com>
2016-09-12 21:32:41 +1000

    Removes sessions form client side code export

--- browser.js
+++ browser.js
@@ -1,5 +1,4 @@
 module.exports = {
-    Sessions: require('./lib/Sessions'),
     Settings: require('./lib/Settings'),
     messageFactory: require('./lib/MessageFactory'),
     constants: require('./lib/Constants')
