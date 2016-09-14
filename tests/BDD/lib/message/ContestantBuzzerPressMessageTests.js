4fa2105e8234eaf00d9bfe1d2d6619166dd87ecf
Wade <wade@boffindevelopments.com>
2016-09-14 18:57:03 +1000

    Fix minor naming erros

--- tests/BDD/lib/message/ContestantBuzzerPressMessageTests.js
+++ tests/BDD/lib/message/ContestantBuzzerPressMessageTests.js
@@ -1,19 +1,19 @@
 /* eslint-disable no-unused-vars */
 var should = require('should');
 /* eslint-enable no-unused-vars */
-var ContestantBuzzerPress = require('../../../../lib/message/ContestantBuzzerPress');
+var ContestantBuzzerPressMessage = require('../../../../lib/message/ContestantBuzzerPressMessage');
 
-describe('ContestantBuzzerPress', function() {
+describe('ContestantBuzzerPressMessage', function() {
     describe('#sessionId', function() {
         it('should set value',
             function() {
-                var m = new ContestantBuzzerPress();
+                var m = new ContestantBuzzerPressMessage();
                 m.sessionId = 'xyz123';
                 m.data._sessionId.should.equal('xyz123');
             });
         it('should get value',
             function() {
-                var m = new ContestantBuzzerPress();
+                var m = new ContestantBuzzerPressMessage();
                 m.data._sessionId = 'xyz123';
                 m.sessionId.should.equal('xyz123');
             });
@@ -21,26 +21,26 @@ describe('ContestantBuzzerPress', function() {
     describe('#contestantId', function() {
         it('should set value',
             function() {
-                var m = new ContestantBuzzerPress();
+                var m = new ContestantBuzzerPressMessage();
                 m.contestantId = 'xyz123';
                 m.data._contestantId.should.equal('xyz123');
             });
         it('should get value',
             function() {
-                var m = new ContestantBuzzerPress();
+                var m = new ContestantBuzzerPressMessage();
                 m.data._contestantId = 'xyz123';
                 m.contestantId.should.equal('xyz123');
             });
     });
     describe('#isValid()', function() {
         it('should return true when valid', function() {
-            var m = new ContestantBuzzerPress();
+            var m = new ContestantBuzzerPressMessage();
             m.sessionId = 'xyz123';
             m.contestantId = 'xyz123';
             m.isValid().should.be.true();
         });
         it('should return false when invalid', function() {
-            var m = new ContestantBuzzerPress();
+            var m = new ContestantBuzzerPressMessage();
             m.sessionId = 'xyz123';
             m.isValid().should.be.false();
         });
